7d9fffb182e1742dc99d7672d66bf113fc8015ac
maxwell-gordon-gogogo <maxwell-gordon@protonmail.com>
2022-04-22 15:42:55 +0800

    exercise 2.1

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Part from './Part';
 
 const Course = ({ course }) => {
   return (
     <div>
-      <p>{course.name}</p>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => (
+        <Part part={part} />
+      ))}
     </div>
   );
 };
